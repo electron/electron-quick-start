81a25b578a2838e81458bdfafd47e9e4a8ffc538
Timothy Cyrus <tcyrus@users.noreply.github.com>
2015-11-23 21:28:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
 // Report crashes to our server.
-require('crash-reporter').start();
+electron.crashReporter.start();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
