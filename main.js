3f26258efd9dcb99e701ab8a7bea8cabbd233e17
Jessica Lord <jlord@github.com>
2015-12-16 11:11:29 -0800

    Use stricter platform check

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let mainWindow;
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
