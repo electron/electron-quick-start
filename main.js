803db72fe6334140f617f2751322189fbc64f95d
Jessica Lord <jlord@github.com>
2015-12-16 11:10:38 -0800

    Restore window on activate event

--- main.js
+++ main.js
@@ -8,11 +8,19 @@ const BrowserWindow = electron.BrowserWindow;  // Module to create native browse
 let mainWindow;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    app.emit('ready');
   }
 });
 
