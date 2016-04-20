ae4e0afaaf1077a7718e34ef9c3f4f95d4b9643c
Jessica Lord <jlord@github.com>
2016-04-20 15:14:27 -0700

    Add a comments

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function createWindow () {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
@@ -47,3 +48,6 @@ app.on('activate', function () {
     createWindow();
   }
 });
+
+// In this file you can include the rest of your app specific main process
+// code. You can also put them in files and require them here. 
