46d7d193cf5c24f2a4560289915a9ee25e9e1ad1
Eduardo San Martin Morote <posva13@gmail.com>
2016-05-12 15:41:16 +0200

    Use string es6 interpolation

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
