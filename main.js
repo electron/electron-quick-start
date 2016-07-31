c616007c2fcaa17fb660a58a64a397f42c273ef4
Anirudh Balaji <anirudhb@users.noreply.github.com>
2016-07-31 12:00:45 -0700

    Use const {}

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+// Module to control application life
+// and browser window
+const { app, BrowserWindow } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
