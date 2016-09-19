5df02a56ce52b45ef99357587105a9bb6781fb69
Antoine Pairet <antoine.pairet@gmail.com>
2016-09-19 22:10:36 +0200

    java-node example

--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+'use strict';
+const java = require('java');
+
+var list1 = java.newInstanceSync("java.util.ArrayList");
+console.log(list1.sizeSync()); // 0
+list1.addSync('item1');
+
+module.exports = list1.sizeSync();
