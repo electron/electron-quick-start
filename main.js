995d109c329fc0ee15d4aba96eb7e6569f3c2c35
Kevin Sawicki <kevinsawicki@gmail.com>
2016-10-31 10:32:20 -0700

    Format URL passed to BrowserWindow.loadURL

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+const url = require('url')
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -13,7 +15,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(url.format({
+    pathname: __dirname + '/index.html',
+    protocol: 'file:',
+    slashes: true
+  }))
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
