6232f81164ff8a933b70f493866fd85bd7c74d12
Jer <jer@jer.com>
2017-06-05 03:07:31 +0100

    next image

--- renderer.js
+++ renderer.js
@@ -9,43 +9,46 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const $ = require('jquery');
-const imgviewport = document.getElementById('imgviewport');
+const next1 = document.getElementById('next1');
+const next2 = document.getElementById('next2');
+const input1 = document.getElementById('imageInput1');
 let currImageList = [];
+let currImagePath = "";
 
-$(function () {
-    $("#imageInput1").change(function () {
-        if (this.files && this.files[0]) {
-            const reader = new FileReader();
-            const imgpath = this.files.item(0).path;
-            const regpattern = /(.*)(\\)(.*)(.png)/;
-            const imgFolder = imgpath.replace(regpattern, '$1');
-            console.log(imgFolder);
-            fs.readdir(imgFolder, function (err, fileslist) {
-                if (fileslist.length > 0) {
-                    debugger;
-                }
+input1.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+        const reader = new FileReader();
+        currImagePath = this.files.item(0).path;
+        const regpattern = /(.*)(\\)(.*)(.png)/;
+        const imgFolder = currImagePath.replace(regpattern, '$1');
+        console.log(imgFolder);
+        currImageList = [];
+        fs.readdir(imgFolder, function (err, fileslist) {
+            if (fileslist.length > 0) {
+                fileslist.forEach(function (val, index) {
+                    currImageList.push(imgFolder + '\\' + val)
+                });
+            }
+        });
+        reader.onload = imageIsLoaded1;
+        next1.addEventListener('click', function (event) {
+            let currImageIndex = currImageList.findIndex(function (ele, index) {
+                return ele === currImagePath;
             });
-            reader.onload = imageIsLoaded1;
-            reader.readAsDataURL(this.files[0]);
-        }
-    });
-        $("#imageInput2").change(function () {
-        if (this.files && this.files[0]) {
-            const reader = new FileReader();
-            const imgpath = this.files.item(0).path;
-            const regpattern = /(.*)(\\)(.*)(.png)/;
-            const imgFolder = imgpath.replace(regpattern, '$1');
-            console.log(imgFolder);
-            fs.readdir(imgFolder, function (err, fileslist) {
-                if (fileslist.length > 0) {
-                    debugger;
-                }
-            });
-            reader.onload = imageIsLoaded2;
-            reader.readAsDataURL(this.files[0]);
-        }
-    });
-});
+            let nextImagePath = 0;
+            if (currImageIndex === 0) {
+                nextImagePath = currImageList[1]
+            } else if (currImageIndex) {
+                nextImagePath = currImageList[currImageIndex + 1]
+            }
+            // debugger;
+            console.log(nextImagePath);
+            currImagePath = nextImagePath;
+            document.getElementById('imgView1').setAttribute('src', nextImagePath);
+        });
+        reader.readAsDataURL(this.files[0]);
+    }
+}, false);
 
 function imageIsLoaded1(e) {
     $('#imgView1').attr('src', e.target.result);
@@ -54,4 +57,3 @@ function imageIsLoaded1(e) {
 function imageIsLoaded2(e) {
     $('#imgView2').attr('src', e.target.result);
 }
-
