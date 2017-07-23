48810a1923c7e42895eb20ec628f2ff78cc31ef4
Graham Smith <smitgw11@gmail.com>
2017-07-22 21:25:46 -0700

    starting point for students

---
+++ reactApp/app.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<p>React glives!</p>,
+   document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './reactApp/app.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /reactApp\/(.)*\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
