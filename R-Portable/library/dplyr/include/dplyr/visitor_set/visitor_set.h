f195a1f07fce3462dc048b6612a4efa8df255d53
Pete Gordon <pete@petegordon.com>
2017-08-09 13:50:32 -0700

    deleted dplyr and BT packages

--- R-Portable/library/dplyr/include/dplyr/visitor_set/visitor_set.h
+++
@@ -1,7 +0,0 @@
-#ifndef dplyr_visitor_set_H
-#define dplyr_visitor_set_H
-
-#include <dplyr/visitor_set/VisitorSetIndexSet.h>
-#include <dplyr/visitor_set/VisitorSetIndexMap.h>
-
-#endif
--- R-Portable/library/dplyr/include/dplyr_types.h
+++
@@ -1,8 +0,0 @@
-#include <dplyr/main.h>
-#include <tools/Quosure.h>
-#include <dplyr/registration.h>
-#include <dplyr/BoolResult.h>
-#include <dplyr/GroupedDataFrame.h>
-
-// avoid inclusion of package header file
-#define dplyr_dplyr_H
