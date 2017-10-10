3a05cc644755ab666250f73db96b9e1bacd9e0d4
peter karasev <karasevpa@gmail.com>
2017-10-10 17:39:49 -0400

    use webgl2

--- webgl-utils.js
+++ webgl-utils.js
@@ -140,13 +140,14 @@ var setupWebGL = function(canvas, opt_attribs, opt_onError) {
  * @return {!WebGLContext} The created context.
  */
 var create3DContext = function(canvas, opt_attribs) {
-  var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+  var names = ["webgl2", "webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
   var context = null;
   for (var ii = 0; ii < names.length; ++ii) {
     try {
       context = canvas.getContext(names[ii], opt_attribs);
     } catch(e) {}
     if (context) {
+      console.log("SUCCESS, got canvas of type:", names[ii]);
       break;
     }
   }
