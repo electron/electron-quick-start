f3226e5e19bbddaacf75b785df86630c53859971
Andrew Hoang <ahoang18@bu.edu>
2018-02-15 16:21:59 -0500

    GPS and temperature working in test

--- renderer.js
+++ renderer.js
@@ -1,9 +1,27 @@
 // TODO retry connection on error
 var ws = new WebSocket('ws://localhost:4000');
 
+var SENSOR_IDS = {
+  'engine-temperature': 1,
+  'latitude': 2,
+  'longitude': 3,
+  'speed': 4
+}
+
 function updateSensors(data) {
-  console.log(data);
-  
+  let { sensor, value, ts } = data;
+  if (!SENSOR_IDS[sensor]) {
+    console.log('Invalid sensor id');
+    return;
+  }
+
+
+  var sensorReadingElementId = 'sensor-' + SENSOR_IDS[sensor] + '-reading';
+  var sensorTimeElementId = 'sensor-' + SENSOR_IDS[sensor] + '-reading-time';
+
+  document.getElementById(sensorReadingElementId).innerText = value;
+  document.getElementById(sensorTimeElementId).innerText = ts;
+
 }
 
 var heartbeatTimeouts = [];
@@ -19,20 +37,27 @@ function registerHeartBeat() {
   var minutes = time.getMinutes();
   var seconds = time.getSeconds();
 
-  document.getElementById('heartbeat-1-status').innerText = 'Serialport ACTIVE.';
+  document.getElementById('heartbeat-1-status').innerText = 'IntelaRacing ACTIVE.';
   document.getElementById('heartbeat-1-time').innerText = hours + ' : ' + minutes + ' : ' + seconds;
 
   heartbeatTimeouts.push(setTimeout(clearHeartBeat, 5000));
 }
 
 function clearHeartBeat() {
-  document.getElementById('heartbeat-1-status').innerText = 'Serialport INACTIVE.';
+  document.getElementById('heartbeat-1-status').innerText = 'IntelaRacing INACTIVE.';
   document.getElementById('heartbeat-1-time').innerText = 'Websocket connection timed out for > 5sec';
   // TODO retry websocket connection
 }
 
 ws.onmessage = function (message) {
-  var data = JSON.parse(message.data);
+  var data;
+  try {
+    data = JSON.parse(message.data);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  
   if (!data) {
     return;
   }
@@ -41,6 +66,8 @@ ws.onmessage = function (message) {
   if (data['heartbeat']) {
     return registerHeartBeat();
   }
+
+  // console.log(data);
 
   updateSensors(data);
 };
