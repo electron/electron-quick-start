7df632c209b3a9f4b3c2346fb1dae338ede6cb20
Chribtoper <xcstolfax@gmail.com>
2018-11-11 21:41:49 -0500

    testing button

--- main.js
+++ main.js
@@ -46,5 +46,6 @@ app.on('activate', function () {
   }
 })
 
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  let button = document.querySelector("#button-1");
+
+  button.addEventListener('click', () => {
+    fetch("")
+
+  })
+
+})
