b239f65379a9efce779c79fc8f573ef7af669ae4
Samuel Attard <sattard@slack-corp.com>
2019-02-11 13:30:34 -0800

    fix: specify nodeIntegration=true for Electron 5 compat

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
