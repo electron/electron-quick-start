a1990d7d219b4d21f2ad6b9cae387cd0f09cf997
Ryan Rowland <ryan.w.rowland@gmail.com>
2019-02-14 16:40:25 -0800

    Include twilio-client

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { Device } = require('twilio-client');
+window.device = new Device();
