7e460bd74d2bccc1adb4e48b278bdf4b701b3150
such <suchen9209@126.com>
2019-04-08 17:21:32 +0800

    去除菜单栏

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
+Menu.setApplicationMenu(null)
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 600, height: 800})
-
+  
   mainWindow.loadFile('login/login.html')
 
 
   // mainWindow.loadFile('cms/index.html')
 
-  //  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
