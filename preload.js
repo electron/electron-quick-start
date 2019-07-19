e81241b9db10ae0481bb95ba487ce9ead3969f63
Shelley Vohr <shelley.vohr@gmail.com>
2019-07-19 14:11:51 -0700

    fix: preload in other pages (#296)

--- preload.js
+++ preload.js
@@ -1,7 +1,12 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  for (const versionType of ['chrome', 'electron', 'node']) {
-    document.getElementById(`${versionType}-version`).innerText = process.versions[versionType]
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  } 
+  
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
   }
 })
