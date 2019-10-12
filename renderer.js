6788309ed8dade0f08f3d054190f44ccb45d0db8
Didrik Nordström <didrik.nordstrom@gmail.com>
2019-10-12 03:59:18 -0700

    Update stale comment: renderer has no access to Node APIs. (#308)

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
