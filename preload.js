deca645e39b4612c6f88864391e46649cb650eb6
Tatsuya Otsuka <1025461+tatzyr@users.noreply.github.com>
2019-12-09 13:51:38 +0900

    chore: remove trailing space (#340)

--- preload.js
+++ preload.js
@@ -4,8 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
-  } 
-  
+  }
+
   for (const type of ['chrome', 'node', 'electron']) {
     replaceText(`${type}-version`, process.versions[type])
   }
