b5b76c5e1cec551aecdbacd0af77f41df8e928f3
Ian Goodacre <Ian.Goodacre@entrain.co.nz>
2020-01-28 14:31:20 +1300

    Two workarounds
    First: start with 'electron --disable-gpu .'
    
    Second: invoke `process.stderr;`
    
    In the latter case, there is no need to save the returned stream or do
    anything with it at all. Merely invoking the method on process is
    sufficient to prevent the error.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+process.stderr;
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
