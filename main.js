dd1bc4a07132062891345889fe05da65bc278953
Jeremy Apthorp <jeremya@chromium.org>
2020-05-11 20:21:58 -0700

    fix: don't listen to 'activate' until ready (#382)
    fixes https://github.com/electron/electron/issues/23476

--- main.js
+++ main.js
@@ -17,6 +17,12 @@ function createWindow () {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 }
 
 // This method will be called when Electron has finished
@@ -29,12 +35,6 @@ app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
