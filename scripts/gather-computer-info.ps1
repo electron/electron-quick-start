3a3c8d0652b3e791c8217d03fc34a03d99a5b1f7
Christopher Bisset <chris@gurucomputing.com.au>
2020-06-09 12:26:20 +1000

    base release

--- scripts/gather-computer-info.ps1
+++ scripts/gather-computer-info.ps1
@@ -1,8 +1,20 @@
 #give off initial info about the computer
 $info = @{}
-$info.model = (get-ciminstance win32_computersystem).manufacturer + " " + (get-ciminstance win32_computersystem).model
-$info.serialnumber = (get-ciminstance win32_bios).SerialNumber
-$info.ram = [string]([int]((get-ciminstance -class "cim_physicalmemory" | ForEach-Object {$_.Capacity} | measure-object -sum).sum / 1GB)) + " GB"
-$info.hddSize = [string][int]((get-ciminstance win32_diskdrive | where-object {$_.index -eq 0}).size / 1000000000) + " GB"
-$info.CPU = (get-ciminstance win32_processor).Name
-$info.GetEnumerator() | Select-Object Key,Value | convertto-html -Fragment
+$info["Model Number"] = (get-ciminstance win32_computersystem).manufacturer + " " + (get-ciminstance win32_computersystem).model
+new-object -type pscustomobject -property $info | convertto-html
+
+$info = @{}
+$info["Serial Number"] = (get-ciminstance win32_bios).SerialNumber
+new-object -type pscustomobject -property $info | convertto-html
+
+$info = @{}
+$info["Ram"] = [string]([int]((get-ciminstance -class "cim_physicalmemory" | ForEach-Object {$_.Capacity} | measure-object -sum).sum / 1GB)) + " GB"
+new-object -type pscustomobject -property $info | convertto-html
+
+$info = @{}
+$info["Size of Hard-Drive"] = [string][int]((get-ciminstance win32_diskdrive | where-object {$_.index -eq 0}).size / 1000000000) + " GB"
+new-object -type pscustomobject -property $info | convertto-html
+
+$info = @{}
+$info["CPU"] = (get-ciminstance win32_processor).Name
+new-object -type pscustomobject -property $info | convertto-html
