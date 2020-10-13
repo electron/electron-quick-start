d2733d666723ae037c0f870aaf298c94cd810db6
Nathaniel Pather <12907920+NathanielPather@users.noreply.github.com>
2020-10-13 21:59:07 +1000

    test

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+// test
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
