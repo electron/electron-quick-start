a67fa80d71014605a432a0d8b3bbf0a4819950a3
Ray Voice <33094591+Ray6464@users.noreply.github.com>
2020-12-08 15:46:33 +0500

    Update main.js
    Added props for remote module enabling in webPreferences. This is required for IPC among renderer processes.

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      // enableRemoteModule: true
     }
   })
 
