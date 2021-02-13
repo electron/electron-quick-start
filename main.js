147ac662cdf833c69089796193dbeb2b02d0599f
Nicolas Stenberg-Daniil <nicolas.stenberg@omneca.com>
2021-02-13 23:24:29 +0100

    SuperVision creation with ElectronJS

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
