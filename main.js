8a3773684de7ab0e6cfeb4668462316ea64cb507
David Strayhorn <david.strayhorn4@gmail.com>
2021-02-23 00:00:59 -0500

    increase window size

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
