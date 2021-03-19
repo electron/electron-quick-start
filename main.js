5d21dc786f7edd240505173bcf2a5c1a7d828ef9
Charles Kerr <charles@charleskerr.com>
2021-03-19 12:28:45 -0500

    feat: first draft of a test template (#469)
    * feat: add a test template

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, crashReporter, ipcMain, BrowserWindow} = require('electron')
 const path = require('path')
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.loadFile('index.html');
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+
   createWindow()
   
   app.on('activate', function () {
@@ -39,5 +33,19 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+function testDone(success, ...logs) {
+  console.log(`test ${success ? 'passed' : 'failed'}`)
+  logs.forEach((i) => console.log(i))
+  process.exit(success ? 0 : 1)
+}
+
+{
+  crashReporter.start({ uploadToServer: false, submitURL: '' })
+  ipcMain.on('test-done', (_, success, ...logs) => testDone(success, ...logs))
+  const failIfBadExit = (details) => {
+    if (details.reason !== 'clean-exit') testDone(false, new Error('trace'), details)
+  }
+  app.on('child-process-gone', (_ev, details) => test.failIfBadExit(details))
+  app.on('render-process-gone', (_ev, _, details) => test.failIfBadExit(details))
+}
--- preload.js
+++ preload.js
@@ -1,12 +1,32 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+// Test helpers
+const test = {
+  assert: (ok, ...logs) => {
+    if (!ok) test.fail(...logs)
+  },
+  fail: (...logs) => test.done(false, ...logs),
+  done: (success = true, ...logs) => {
+    if (!success) logs.unshift(new Error('test failed'))
+    require('electron').ipcRenderer.send('test-done', success, ...logs)
+  },
+}
+
+// Example test: check that process.versions.electron
+// - is defined in the preload process
+// - has major, minor, and patch numbers
+// - the numbers are non-negative
+try {
+  const ver = process.versions.electron
+  const tokens = process.versions.electron.split('.', 3)
+  test.assert(tokens.length === 3)
+  for (const token of tokens) {
+    const num = Number.parseInt(token)
+    test.assert(num !== NaN)
+    test.assert(num >= 0)
   }
-})
+  test.done()
+} catch (err) {
+  test.fail(err)
+}
