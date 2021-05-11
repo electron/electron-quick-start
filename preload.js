8171bbb56ff7d9e7775cc90e2cb0d365771be1c8
Erick Zhao <erick@hotmail.ca>
2021-05-11 10:47:34 -0700

    chore: update comments in quick-start

--- preload.js
+++ preload.js
@@ -1,12 +1,15 @@
 // All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+// The preload script runs in an isolated world by default, similar
+// to content scripts in a Chrome Extension.
+// https://developer.chrome.com/docs/extensions/mv3/content_scripts/#isolated_world
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
   }
 })
--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
-// This file is required by the index.html file and will
+// This file is included by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+// `nodeIntegration` is turned off and `contextIsolation`
+// is turned on. Use `preload.js` to selectively enable
+// features needed in the renderer process.
