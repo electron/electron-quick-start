eac735f34eb6e4bd1d8d6ce1122986b2aeff51e3
detronetdip <ayondip2001@gmail.com>
2022-02-07 00:52:02 +0530

    `contextBridge` example added

--- preload.js
+++ preload.js
@@ -1,12 +1,25 @@
+// Isolated World
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const { contextBridge } = require("electron");
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
   }
-})
+});
+
+// All apis from preload script will be available
+// inside renderer process via contextbridge.
+contextBridge.exposeInMainWorld("electron", {
+  newApi: () => {
+    return "Hello from main process";
+  },
+});
+// Access apis in rederer proses by
+// window.electron.newApi();
