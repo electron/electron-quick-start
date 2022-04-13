70303d8ec9624c4c609bbe1315372e555eee0d6e
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-04-13 12:51:44 +0200

    enhancement: easier read

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
+// Node.js APIs are unavailable in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
