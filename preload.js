9d025074d9571f8a03562e28f5c77edcc28d5899
David Sanders <dsanders11@ucsbalum.com>
2023-04-21 00:38:24 -0700

    chore: lint JS with standard (#769)

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@
  * The preload script runs before. It has access to web APIs
  * as well as Electron's renderer process modules and some
  * polyfilled Node.js functions.
- * 
+ *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
 window.addEventListener('DOMContentLoaded', () => {
