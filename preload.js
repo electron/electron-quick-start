f397dc74e9c032903717ea5f223f307e7652888b
Anny Yang <yangannyx@gmail.com>
2024-02-26 13:50:47 -0800

    fix: clarifies what preload script runs before (#892)

--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
 /**
- * The preload script runs before. It has access to web APIs
- * as well as Electron's renderer process modules and some
- * polyfilled Node.js functions.
+ * The preload script runs before `index.html` is loaded
+ * in the renderer. It has access to web APIs as well as
+ * Electron's renderer process modules and some polyfilled
+ * Node.js functions.
  *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
